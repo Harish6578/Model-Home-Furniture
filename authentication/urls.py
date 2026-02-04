862fa4b93e7a68eda796c1ea7d1c3052a70656de
Harish6578 <harishvarma6578@gmail.com>
2026-02-04 11:02:24 +0530

    Updated

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from .views import UserRegisterView,UserLoginView
 
+# Password Reset
+
+from .views import sent_otp_mail
+
 urlpatterns=[
     path('register/',UserRegisterView.as_view(),name='signup'),
-    path('login/',UserLoginView.as_view(),name='signin')
+    path('login/',UserLoginView.as_view(),name='signin'),
+    path('send-otp/',sent_otp_mail,name='send_otp')
 ]
